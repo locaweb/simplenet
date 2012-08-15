17b79512f7128585e46aa6dbb265da0a86b5d554
Juliano Martinez <juliano.martinez@locaweb.com.br>
2012-08-15 13:53:31 -0300

    lalala

--- simplenet/network_appliances/base.py
+++ simplenet/network_appliances/base.py
@@ -22,53 +22,171 @@ from simplenet.views.format_view import FormatView
 class SimpleNet(object):
 
     def __init__(self):
-        self.connection = False
         self.format_for = FormatView()
 
-    def neighborhood_list(self, *args, **kawrgs):
-        raise FeatureNotImplemented()
+    def neighborhood_list(self):
+        ss = session.query(models.Neighborhood).all()
+        neighborhoods = []
+        for neighborhood in ss:
+            neighborhoods.append(
+                self.format_for.neighborhood(
+                    neighborhood.id,
+                    neighborhood.name
+                )
+            )
+        return neighborhoods
 
-    def neighborhood_create(self, *args, **kawrgs):
-        raise FeatureNotImplemented()
-
-    def neighborhood_info(self, *args, **kawrgs):
-        raise FeatureNotImplemented()
+    def neighborhood_create(self, data):
+        if not 'name' in data:
+            raise Exception('Missing name on request')
+        session.begin(subtransactions=True)
+        try:
+            session.add(models.Neighborhood(name=data['name']))
+            session.commit()
+        except IntegrityError:
+            session.rollback()
+            forbidden_msg = "%s already exists" % data['name']
+            raise OperationNotPermited('Neighborhood', forbidden_msg)
+        except Exception, e:
+            session.rollback()
+            raise Exception(e)
+        return self.neighborhood_info_by_name(data['name'])
 
     def neighborhood_update(self, *args, **kawrgs):
         raise FeatureNotImplemented()
 
-    def neighborhood_delete(self, *args, **kawrgs):
-        raise FeatureNotImplemented()
+    def neighborhood_info(self, id):
+        ss = session.query(models.Neighborhood).get(id)
+        if not ss:
+            raise EntityNotFound('Neighborhood', id)
+        return self.format_for.neighborhood(ss.id, ss.name)
 
-    def vlan_list(self, *args, **kawrgs):
-        raise FeatureNotImplemented()
+    def neighborhood_info_by_name(self, name):
+        ss = session.query(models.Neighborhood).filter_by(name=name).first()
+        if not ss:
+            raise EntityNotFound('Neighborhood', name)
+        return self.format_for.neighborhood(ss.id, ss.name)
 
-    def vlan_create(self, *args, **kawrgs):
-        raise FeatureNotImplemented()
+    def neighborhood_delete(self, id):
+        ss = session.query(models.Neighborhood).get(id)
+        session.begin(subtransactions=True)
+        try:
+            session.delete(ss)
+            session.commit()
+        except Exception, e:
+            session.rollback()
+            raise Exception(e)
+        return True
 
-    def vlan_info(self, *args, **kawrgs):
-        raise FeatureNotImplemented()
+    def vlan_list(self):
+        ss = session.query(models.Vlan).all()
+        vlans = []
+        for vlan in ss:
+            vlans.append(
+                self.format_for.vlan(
+                    vlan.id,
+                    vlan.name,
+                    vlan.neighborhood_id
+                )
+            )
+        return vlans
+
+    def vlan_create(self, neighborhood_id, data):
+        if not 'name' in data:
+            raise Exception('Missing name on request')
+        session.begin(subtransactions=True)
+        try:
+            session.add(models.Vlan(name=data['name'], neighborhood_id=neighborhood_id))
+            session.commit()
+        except IntegrityError:
+            session.rollback()
+            forbidden_msg = "%s already exists" % data['name']
+            raise OperationNotPermited('Vlan', forbidden_msg)
+        except Exception, e:
+            session.rollback()
+            raise Exception(e)
+        return self.vlan_info_by_name(data['name'])
+
+    def vlan_info(self, id):
+        ss = session.query(models.Vlan).get(id)
+        if not ss:
+            raise EntityNotFound('Vlan', id)
+        return self.format_for.vlan(ss.id, ss.name)
+
+    def vlan_info_by_name(self, name):
+        ss = session.query(models.Vlan).filter_by(name=name).first()
+        if not ss:
+            raise EntityNotFound('Vlan', name)
+        return self.format_for.vlan(ss.id, ss.name, ss.neighborhood_id)
 
     def vlan_update(self, *args, **kawrgs):
         raise FeatureNotImplemented()
 
-    def vlan_delete(self, *args, **kawrgs):
+    def vlan_delete(self, id):
+        ss = session.query(models.Vlan).get(id)
+        session.begin(subtransactions=True)
+        try:
+            session.delete(ss)
+            session.commit()
+        except Exception, e:
+            session.rollback()
+            raise Exception(e)
+        return True
+
+    def subnet_list(self):
+        ss = session.query(models.Subnet).all()
+        subnets = []
+        for subnet in ss:
+            subnets.append(
+                self.format_for.subnet(
+                    subnet.id,
+                    subnet.cidr,
+                    subnet.vlan_id
+                )
+            )
+        return subnets
+
+    def subnet_create(self, vlan_id, data):
+        if not 'cidr' in data:
+            raise Exception('Missing cidr on request')
+        session.begin(subtransactions=True)
+        try:
+            session.add(models.Subnet(cidr=data['cidr'], vlan_id=vlan_id))
+            session.commit()
+        except IntegrityError:
+            session.rollback()
+            forbidden_msg = "%s already exists" % data['cidr']
+            raise OperationNotPermited('Subnet', forbidden_msg)
+        except Exception, e:
+            session.rollback()
+            raise Exception(e)
+        return self.subnet_info_by_cidr(data['cidr'])
+
+    def subnet_info(self, id):
+        ss = session.query(models.Subnet).get(id)
+        if not ss:
+            raise EntityNotFound('Subnet', id)
+        return self.format_for.subnet(ss.id, ss.cidr, ss.vlan_id)
+
+    def subnet_info_by_cidr(self, cidr):
+        ss = session.query(models.Subnet).filter_by(cidr=cidr).first()
+        if not ss:
+            raise EntityNotFound('Subnet', cidr)
+        return self.format_for.subnet(ss.id, ss.cidr, ss.vlan_id)
+
+    def subnet_update(self, *args, **kwargs):
         raise FeatureNotImplemented()
 
-    def subnet_list(self, *args, **kawrgs):
-        raise FeatureNotImplemented()
-
-    def subnet_create(self, *args, **kawrgs):
-        raise FeatureNotImplemented()
-
-    def subnet_info(self, *args, **kawrgs):
-        raise FeatureNotImplemented()
-
-    def subnet_update(self, *args, **kawrgs):
-        raise FeatureNotImplemented()
-
-    def subnet_delete(self, *args, **kawrgs):
-        raise FeatureNotImplemented()
+    def subnet_delete(self, id):
+        ss = session.query(models.Subnet).get(id)
+        session.begin(subtransactions=True)
+        try:
+            session.delete(ss)
+            session.commit()
+        except Exception, e:
+            session.rollback()
+            raise Exception(e)
+        return True
 
     def ip_list(self, *args, **kawrgs):
         raise FeatureNotImplemented()
