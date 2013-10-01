8fca54756acebce990186cf0099dc36706a08d94
Juliano Martinez <juliano@martinez.io>
2013-10-01 10:41:58 -0300

    added

---
+++ src/simplenet/common/hooks.py
@@ -0,0 +1,45 @@
+# Copyright 2013 Locaweb.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# @author: Juliano Martinez (ncode), Locaweb.
+
+import os
+from functools import wraps
+
+def pre_run():
+    def proxy(f):
+        @wraps(f)
+        def pre(*args, **kwargs):
+            if os.path.isfile("/etc/simplenet/hooks/pre_%s.py" % f.__name__):
+                ## run the hook
+                return f(*args, **kwargs)
+            return f(*args, **kwargs)
+        return pre
+    return proxy
+
+def post_run():
+    def proxy(f):
+        @wraps(f)
+        def pre(*args, **kwargs):
+            if os.path.isfile("/etc/simplenet/hooks/pre_%s.py" % f.__name__):
+                try:
+                    result = f(*args, **kwargs)
+                except Exception, e:
+                    raise Exception(e) 
+                ## run the hook 
+                return result
+            return f(*args, **kwargs)
+        return pre
+    return proxy
